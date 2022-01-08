d4c0e123c106b80a11bfb4085e334253bf37cff4
Hugo Feldstein <16608904+Taloud@users.noreply.github.com>
2022-01-09 00:41:23 +0100

    feat(lsp): add auto install lsp server if not present

--- nvim/.config/nvim/lua/taloud/lsp/lsp-installer.lua
+++ nvim/.config/nvim/lua/taloud/lsp/lsp-installer.lua
@@ -1,27 +1,49 @@
-local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+local status_ok, lsp_installer_servers = pcall(require, "nvim-lsp-installer.servers")
 if not status_ok then
   return
 end
 
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
-lsp_installer.on_server_ready(function(server)
-  local opts = {
-    on_attach = require("taloud.lsp.handlers").on_attach,
-    capabilities = require("taloud.lsp.handlers").capabilities,
-  }
+-- Auto install used server
+local lspServers = {
+  ansiblels = {},
+  cssls = {},
+  diagnosticls = {},
+  dockerls = {},
+  eslint = {},
+  html = {},
+  intelephense = {},
+  jsonls = {},
+  phpactor = {},
+  sumneko_lua = {},
+  tsserver = {},
+  vimls = {},
+  yamlls = {},
+}
 
-   if server.name == "jsonls" then
-     local jsonls_opts = require("taloud.lsp.settings.jsonls")
-     opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-   end
+for lspServer, opts in pairs(lspServers) do
+  local server_available, requested_server = lsp_installer_servers.get_server(lspServer)
+  if server_available then
+      requested_server:on_ready(function (server)
+          local opts = {
+            on_attach = require("taloud.lsp.handlers").on_attach,
+            capabilities = require("taloud.lsp.handlers").capabilities,
+          }
 
-   if server.name == "sumneko_lua" then
-     local sumneko_opts = require("taloud.lsp.settings.sumneko_lua")
-     opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-   end
+         if server.name == "jsonls" then
+           local jsonls_opts = require("taloud.lsp.settings.jsonls")
+           opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+         end
 
-  -- This setup() function is exactly the same as lspconfig's setup function.
-  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-  server:setup(opts)
-end)
+         if server.name == "sumneko_lua" then
+           local sumneko_opts = require("taloud.lsp.settings.sumneko_lua")
+           opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+         end
+
+          requested_server:setup(opts)
+      end)
+      if not requested_server:is_installed() then
+          -- Queue the server to be installed
+          requested_server:install()
+      end
+  end
+end
