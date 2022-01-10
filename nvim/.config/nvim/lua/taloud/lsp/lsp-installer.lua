74e51a7700066bce7d5b20c46306019cf131e729
Taloud <16608904+Taloud@users.noreply.github.com>
2022-01-10 10:34:20 -0800

    feat(lua): add vim to global

--- nvim/.config/nvim/lua/taloud/lsp/lsp-installer.lua
+++ nvim/.config/nvim/lua/taloud/lsp/lsp-installer.lua
@@ -14,7 +14,15 @@ local lspServers = {
   intelephense = {},
   jsonls = {},
   -- phpactor = {} only use intelephense for now,
-  sumneko_lua = {},
+  sumneko_lua = {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = {'vim'},
+        }
+      }
+    }
+  },
   tsserver = {},
   vimls = {},
   yamlls = {},
@@ -24,10 +32,10 @@ for lspServer, opts in pairs(lspServers) do
   local server_available, requested_server = lsp_installer_servers.get_server(lspServer)
   if server_available then
       requested_server:on_ready(function (server)
-          local opts = {
+          table.insert(opts, {
             on_attach = require("taloud.lsp.handlers").on_attach,
             capabilities = require("taloud.lsp.handlers").capabilities,
-          }
+          })
 
           requested_server:setup(opts)
       end)
